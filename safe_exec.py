d0e7f395518722597f700a52a6679edc8966450c
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2016-08-13 18:11:23 -0700

    execute successfully returns stdout

--- safe_exec.py
+++ safe_exec.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 from docker import Client
 
+
 def execute(workdir, data, stdin):
     # create the client to the docker service
     cli = Client(base_url='unix://var/run/docker.sock')
@@ -9,28 +10,29 @@ def execute(workdir, data, stdin):
     # generate the temporary path for the worker
     with tempfile.TemporaryDirectory(dir=workdir) as dirpath:
         print(dirpath)
-        return
+
         # create the input file
         with open(os.path.join(dirpath, 'input.py'), 'w') as f:
             f.write(data)
 
         host_config = cli.create_host_config(
-                binds=['{}:/tmp'.format(dirpath)])
+                binds=['{}/:/mnt/data'.format(dirpath)])
 
         container = cli.create_container(
-                volumes=['/tmp'],
+                volumes=['/mnt/data'],
                 image='alpine-python2:latest',
-                command='python /tmp/input.py > /tmp/output.txt',
+                command='python /mnt/data/input.py',
                 host_config=host_config )
 
-        response = cli.start(container=container.get('Id'))
-        with open(os.path.join(dirpath, 'output.txt')) as f:
-                print(f.read())
+        c_id = container.get('Id')
+        response = cli.start(container=c_id)
+        output = cli.logs(container=c_id, stdout=True)
+        print(output)
 
 
 if __name__ == '__main__':
     workdir = os.path.join(os.getcwd(), 'tmp')
-    script = 'print("hello")'
+    script = 'for i in range(10):\n\tprint("hello")'
     stdin = None
 
     execute(workdir, script, stdin)
