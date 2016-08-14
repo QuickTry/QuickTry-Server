8deb1edc8f73914ac40c6346630cb318ac2a5d56
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2016-08-14 09:50:51 -0700

    Add a run script and requirements file

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This is a run script for development. This assumes that you are running the
+# correct python environment, along with the docker service.
+# `rebuild_docker.sh` should be called before running this script to ensure
+# that the correct docker images exist for all supported languages. We run the
+# flask application as localhost with debug enabled and forward the traffic
+# over ngrok. This is not suggested for anything more rigorous than a
+# hackathon!
+
+set -o errexit
+set -o xtrace
+
+tmpdir="tmp"
+
+# perform cleanup on interrupt
+# TODO: kill all running docker containers containing quicktry
+trap cleanup INT
+function cleanup() {
+    rm -r $tmpdir
+}
+
+# create the working directory for mounting docker volumes
+if [ ! -d "$tmpdir" ]; then
+    mkdir $tmpdir
+fi
+
+# run the flask application
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+python -m flask run
