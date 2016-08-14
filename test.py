8b8f096fe70b64ed02224aaa027e6a6e7df40131
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2016-08-13 18:47:06 -0700

    Restructure project to reflect its usage as a library

---
+++ test.py
@@ -0,0 +1,9 @@
+from quicktry import quicktry
+import os
+
+workdir = os.path.join(os.getcwd(), 'tmp')
+script = 'for i in range(10):\n\tprint("hello")'
+stdin = None
+
+output = quicktry.execute(workdir, script, stdin)
+print(output)
